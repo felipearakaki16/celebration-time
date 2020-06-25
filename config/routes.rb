7e5579705a1b4bff63736ce13ff9c8068c627fb4
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-25 12:32:06 -0300

    Fixed cocktail routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/cocktails', to: 'cocktails#index'
-  get '/cocktails/:id', to: 'cocktails#show'
-  get '/cocktails/:id', to: 'cocktails#new'
+  get '/cocktails', to: 'cocktails#index', as: 'cocktail'
+  get '/cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
   post '/cocktails', to: 'cocktails#create'
+  get '/cocktails/:id', to: 'cocktails#show'
 end
+
+
+
+
+
+#      Prefix Verb URI                      Pattern                                                                              Controller#Action
+#   cocktails GET  /cocktails(.:format)      cocktails#index
+#             GET  /cocktails/:id(.:format)  cocktails#show
+#             GET  /cocktails/:id(.:format)  cocktails#new
+#             POST /cocktails(.:format)      cocktails#create
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,25 @@
+puts 'Deleting existing ingredients'
+Ingredient.destroy_all
+puts 'Creating new ingredients'
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Sugar syrup")
+Ingredient.create(name: "Ginger")
+Ingredient.create(name: "Cocoa powder")
+Ingredient.create(name: "Bitters")
+Ingredient.create(name: "Coffee liqueur")
+Ingredient.create(name: "Blended whiskey")
+Ingredient.create(name: "Sweet Vermouth")
+Ingredient.create(name: "Apple brandy")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Peppermint schnapps")
+Ingredient.create(name: "Lager")
+Ingredient.create(name: "Carbonated water")
+Ingredient.create(name: "Sparkling wine")
+Ingredient.create(name: "Berries")
+Ingredient.create(name: "Scotch")
+Ingredient.create(name: "Pisco")
+puts 'Ingredients created!'
