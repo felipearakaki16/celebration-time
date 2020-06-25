9847cef84bc3182158b5f463acb483d445dc0d31
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-25 11:26:53 -0300

    Cocktails controller created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/cocktails', to: 'cocktails#index'
+  get '/cocktails/:id', to: 'cocktails#show'
+  get '/cocktails/:id', to: 'cocktails#new'
+  post '/cocktails', to: 'cocktails#create'
 end
