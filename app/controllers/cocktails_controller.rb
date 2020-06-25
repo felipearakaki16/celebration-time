d27e273d66239579c44a762fb2766aeeee632d70
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-25 11:59:10 -0300

    Cocktails methods and views created.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,35 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail), notice: 'Cocktail successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "Allow list" through.
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
