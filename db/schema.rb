3a8203714389fe121b2edf8d3afc376072ab10ef
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-25 10:47:09 -0300

    Redoing 3 models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_130950) do
+ActiveRecord::Schema.define(version: 2020_06_25_134620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2020_06_25_130950) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.text "description"
     t.bigint "cocktail_id", null: false
     t.bigint "ingredient_id", null: false
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
